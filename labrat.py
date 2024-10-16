142363d5ffa08c9dcad71d3a26ade5b124a0fcb9
theorjiugovictor <orjiugo.victor@gmail.com>
2024-10-16 12:49:00 +0200

    A python code to collect input and provide feedbac

---
+++ labrat.py
@@ -0,0 +1,7 @@
+#variable is used for storing data, strings are stored in double quotes
+name = input("What is your name?")
+date = input("Input your preferred Date")
+time = input ("Input your preferred Time")
+feedback = "Dear "+ name +" your appointment is scheduled for, "+ date +" at "+ time +". Do well to be at the premises 30 minutes fbefore your scheduled time"
+#print function is used to display text in the screen 
+print(feedback)
